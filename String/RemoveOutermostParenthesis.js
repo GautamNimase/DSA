9f78a6617809389cb143205fa961bdfee313c903
Gautam <gautamnimase007@gmail.com>
2025-08-23 23:44:39 +0530

    string problems

---
+++ String/RemoveOutermostParenthesis.js
@@ -0,0 +1,26 @@
+/**
+Remove Outermost Parentheses
+ */
+var removeOuterParentheses = function(s) {
+    let result=""
+    let depth=0
+
+
+    
+    for(let char of s){
+        if(char==="("){
+            if(depth>0){
+                result+=char;
+            }
+            depth++;
+        }
+        else{
+            depth--;
+            if(depth>0){
+                result+=char;
+            }
+
+        }
+    }
+    return result;
+}
---
+++ String/ReverseWordsInAString.js
@@ -0,0 +1,13 @@
+// Reverse Words in a String
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function reverseWords(s) {
+    return s
+        .trim()                // remove outer spaces
+        .split(/\s+/)          // split by 1 or more spaces
+        .reverse()             // reverse the array
+        .join(" ");            // join back with single spaces
+}
