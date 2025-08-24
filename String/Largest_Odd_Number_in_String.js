0e7f1241f1bd809c95baf8fec8e92bbbd92e20c5
Gautam <gautamnimase007@gmail.com>
2025-08-24 08:08:21 +0530

    DSA

---
+++ String/Largest_Odd_Number_in_String.js
@@ -0,0 +1,18 @@
+// Largest Odd Number in String
+
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var largestOddNumber = function(num) {
+    let result=""
+
+    for(let i=num.length-1;i>=0;i--){
+        if(parseInt(num[i])%2===1){
+            return num.substring(0,i+1)
+        }
+        
+    }
+    return ""
+    
+};
